a8203aa6e939a1332402127ea94795aa20a4b898
Your Name <you@example.com>
2024-07-01 16:08:34 +0100

    basic layout and header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
     @vite(['resources/css/app.css','resources/js/app.js'])
     @inertiaHead
 </head>
